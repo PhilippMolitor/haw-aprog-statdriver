67dfe300d22ff9dda89f72a686dcf5af176e056b
PhilsLab <15452526+PhilsLab@users.noreply.github.com>
2020-05-11 22:06:31 +0200

    add scoreboard route with GET implemented and basic statistics

---
+++ src/controllers/scoreboard.js
@@ -0,0 +1,109 @@
+const r = require('express').Router();
+
+// creates a new scoreboard
+r.post('/', (req, res) => {
+    const { gameId, scoreboardName } = req.body;
+
+});
+
+// shows a scoreboard
+r.get('/:scoreboardId', (req, res) => {
+    const { scoreboardId } = req.params;
+    const { maxEntries, pastDays } = req.query;
+
+    // check for scoreboard
+    const stmt = req.database
+        .prepare(`SELECT name          as scoreboardName,
+                         game_id       as gameId,
+                         set_key       as setKey,
+                         get_key       as getKey,
+                         embed_enabled as embedEnabled,
+                         embed_title   as embedTitle
+                  FROM scoreboards
+                  WHERE scoreboard_id = @id`);
+    const result = stmt
+        .get({
+            id: scoreboardId
+        });
+    const { scoreboardName, gameId, setKey, getKey, embedEnabled, embedTitle } = result;
+
+    if (result) {
+        // game name
+        const gameStmt = req.database
+            .prepare(`SELECT name as gameName
+                      FROM games
+                      WHERE game_id = @id`);
+        const { gameName } = gameStmt
+            .get({
+                id: gameId
+            });
+
+        // entries
+        const entryStmt = req.database
+            .prepare(`SELECT entry_id    as id,
+                             player_name as name,
+                             score       as score,
+                             date        as timestamp
+                      FROM entries
+                      WHERE scoreboard_id = @id
+                      ORDER BY score DESC
+                      LIMIT @max`);
+        const entries = entryStmt.all({
+            id: scoreboardId,
+            max: parseInt(maxEntries) || 10
+        });
+
+        // limited stats
+        const statsLimitedStmt = req.database
+            .prepare(`SELECT COUNT(*)   as timeScoreCount,
+                             AVG(score) as timeScoreAverage
+                      FROM entries
+                      WHERE scoreboard_id = @id
+                        AND date > @minTime`);
+        const { timeScoreCount, timeScoreAverage } = statsLimitedStmt.get({
+            id: scoreboardId,
+            minTime: (new Date().getTime() / 1000) - ((parseInt(pastDays) || 7) * 60 * 60 * 24)
+        });
+
+        // all-time stats
+        const statsAllStmt = req.database
+            .prepare(`SELECT COUNT(*)   as allScoreCount,
+                             AVG(score) as allScoreAverage
+                      FROM entries
+                      WHERE scoreboard_id = @id`);
+        const { allScoreCount, allScoreAverage } = statsAllStmt.get({
+            id: scoreboardId
+        });
+
+        // render results
+        res.render('scoreboard', {
+            details: {
+                game: gameName,
+                scoreboard: scoreboardName,
+                setKey, getKey,
+                embedEnabled, embedTitle
+            },
+            stats: {
+                timeScoreAverage, timeScoreCount,
+                allScoreAverage, allScoreCount
+            },
+            entries,
+        });
+    } else {
+        res.send('404 scoreboard not found');
+    }
+});
+
+// changes details about a scoreboard
+r.post('/:scoreboardId', (req, res) => {
+    const { scoreboardId } = req.params;
+    const { newName, enableEmbed, resetApiKeys, clearEntries, deleteScore } = req.body;
+});
+
+// removes a scoreboard from the database
+r.delete('/:scoreboardId', (req, res) => {
+    const { scoreboardId } = req.params;
+
+});
+
+module.exports = r;
--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ const r = require('express').Router();
 // route controllers
 const authController = require('./controllers/auth');
 const dashboardController = require('./controllers/dashboard');
+const scoreboardController = require('./controllers/scoreboard');
 const embedController = require('./controllers/embed');
 const apiController = require('./controllers/api');
 
@@ -15,6 +16,7 @@ r.get('/', (req, res) => {
 // controllers
 r.use('/auth', authController);
 r.use('/dashboard', dashboardController);
+r.use('/scoreboard', scoreboardController);
 r.use('/embed', embedController);
 r.use('/api', apiController);
 
