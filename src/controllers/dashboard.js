4c276f9cc00f05f295598f7424a6a7c0508ca0d8
PhilsLab <15452526+PhilsLab@users.noreply.github.com>
2020-05-17 23:41:33 +0200

    add game short statistics

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -3,17 +3,21 @@ const r = require('express').Router();
 // dashboard template (get)
 r.get('/', (req, res) => {
     const stmt = req.database
-        .prepare(`SELECT game_id as id,
-                         name    as name
-                  FROM games
+        .prepare(`SELECT g.game_id                       AS id,
+                         g.name                          AS name,
+                         COUNT(DISTINCT s.scoreboard_id) AS scoreboardCount,
+                         COUNT(DISTINCT e.entry_id)      AS entryCount
+                  FROM games g
+                           JOIN scoreboards s ON g.game_id = s.game_id
+                           JOIN entries e ON s.scoreboard_id = e.scoreboard_id
                   WHERE owner_id = @owner`);
     const result = stmt.all({
-        owner: req.authentication.getUserId()
+        owner: req.authentication.getUserId(),
     });
 
     res.render('dashboard', {
-        games: result
-    })
+        games: result,
+    });
 });
 
 // dashboard new game form (get)
@@ -23,14 +27,14 @@ r.get('/new-game', (req, res) => {
 
 // dashboard new game handler (post)
 r.post('/new-game', (req, res) => {
-    const {gameName} = req.body;
+    const { gameName } = req.body;
 
     const stmt = req.database
         .prepare(`INSERT INTO games (owner_id, name)
                   VALUES (@owner, @gameName)`);
     const result = stmt.run({
         owner: req.authentication.getUserId(),
-        gameName
+        gameName,
     });
 
     res.redirect('/game/' + result.lastInsertRowid);
