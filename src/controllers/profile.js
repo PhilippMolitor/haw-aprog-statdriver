f9ea246d3d36c260b191a5258049ebac38eb2602
Jonas <57606077+N1Keee@users.noreply.github.com>
2020-05-19 19:32:09 +0200

    fixed previous password-check

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -16,33 +16,48 @@ r.get('/', (req, res) => {
 
 // change password get-route
 r.get('/change-password', (req, res) => {
-    res.render('change-password');
+    const {error} = req.query;
+
+    res.render('change-password', {error});
 });
 
 //change password post-route
 r.post('/change-password', (req, res) => {
     const {password, newPassword, newPasswordConfirm} = req.body;
 
-    if (newPassword == newPasswordConfirm) {
-        // creating a new password ... if
-        if (newPassword !== password) {
-            //TODO: update new password
-            const stmt = req.database
-                .prepare(`UPDATE users
-                          SET password_hash = @newPassword
-                          WHERE user_id = @id`);
-            stmt.run({
-                newPassword: bcrypt.hashSync(newPassword, 12),
-                id: req.authentication.getUserId()
-            });
-            //redirect to profile-page
-            res.redirect('/profile');
+    const stmt = req.database
+        .prepare(`SELECT password_hash AS passwordHash
+                  FROM users
+                  WHERE user_id = @id`);
+
+    const result = stmt.get({
+        id: req.authentication.getUserId()
+    });
+
+
+    if (!bcrypt.compareSync(password, result.passwordHash)) {
+        res.redirect('/profile/change-password?error=1')
+    } else {
+        if (newPassword === newPasswordConfirm) {
+            // creating a new password ... if
+            if (newPassword !== password) {
+                const stmt = req.database
+                    .prepare(`UPDATE users
+                              SET password_hash = @newPassword
+                              WHERE user_id = @id`);
+                stmt.run({
+                    newPassword: bcrypt.hashSync(newPassword, 12),
+                    id: req.authentication.getUserId()
+                });
+                //redirect to profile-page
+                res.redirect('/profile');
+            } else {
+                // same password
+                res.redirect('/profile/change-password?error=3');
+            }
         } else {
-            // same password
             res.redirect('/profile/change-password?error=2');
         }
-    } else {
-        res.redirect('/profile/change-password?error=1');
     }
 });
 
